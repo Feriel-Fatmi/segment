e1514f8307ea74b540fcb2f03061cefffbe86db0
Asmaa Bengueddach <asmaa.bengueddach@gmail.com>
2016-10-29 20:10:17 +0200

    voir constructeur par défaut

--- src/point.java
+++ src/point.java
@@ -4,9 +4,9 @@
 public class point {
     private double x;
     private double y;
-    public point(){
-        this.x=x;
-        this.y=y;
+    public point(){/*x=0; y=0*/
+        this.x=0;
+        this.y=0;
     }
     public point (double x){
         this.x=x;
